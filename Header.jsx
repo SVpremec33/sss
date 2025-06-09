87592d4563ed50fa849ae0429c38f739a62bbbff
SVpremec33 <0.2twinktomi16@gmail.com>
2025-06-09 11:45:11 +0600

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,24 @@
+import logo from "../assets/images/logo.svg"
+import { Link } from "react-router-dom";
+import {CART} from "../utils/const";
+import { useCart } from "../context/CartContext";
+
+
+function Header(){
+
+    const {totalQuantity} = useCart();
+
+    return (
+        <header className="header">
+            <div className="container header-flex">
+                <div className="logo">
+                <img src={logo} alt="Logo" />
+                <span className="logo-text">Pizzafy.</span>
+                </div>
+            <Link to={CART} className="cart-button">Корзина : {totalQuantity}</Link>
+            </div>
+        </header>
+    );
+  }
+  
+  export default Header;
